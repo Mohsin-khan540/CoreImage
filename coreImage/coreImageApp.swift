e2334930d804537bd209b9f42e96de11008d2c54
Mohsin-khan540 <arhamk4555@gmail.com>
2025-10-18 12:17:40 +0500

    Initial Commit

---
+++ coreImage/coreImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  coreImageApp.swift
+//  coreImage
+//
+//  Created by Mohsin khan on 18/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct coreImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ coreImageTests/coreImageTests.swift
@@ -0,0 +1,17 @@
+//
+//  coreImageTests.swift
+//  coreImageTests
+//
+//  Created by Mohsin khan on 18/10/2025.
+//
+
+import Testing
+@testable import coreImage
+
+struct coreImageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
